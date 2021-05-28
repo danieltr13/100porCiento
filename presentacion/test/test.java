908b9ea6eaade108d14e777467669396f397fbf8
Brayan-Alexis <brayanzxd@gmail.com>
2021-05-28 15:22:07 -0700

    Ingredientes/productos

--- presentacion/test/test.java
+++ presentacion/test/test.java
@@ -43,7 +43,7 @@ public class test {
         
         clientes.guardar(cliente);
         
-        //Cafe
+        //Licuados
         Ingrediente platano = new Ingrediente("Platano", Float.valueOf(5));
         Ingrediente azucar = new Ingrediente("Azucar", Float.valueOf(0));
         Ingrediente canela = new Ingrediente("Canela", Float.valueOf(0));
@@ -52,6 +52,10 @@ public class test {
         Ingrediente arandono = new Ingrediente("Arandono", Float.valueOf(0));
         Ingrediente frutosRojos = new Ingrediente("Frutos Rojos", Float.valueOf(0));
         
+        //Cafe
+        Ingrediente agua = new Ingrediente("Agua", Float.valueOf(0));
+        Ingrediente cafe = new Ingrediente("Cafe", Float.valueOf(0));
+        Ingrediente leche = new Ingrediente("Leche", Float.valueOf(0));
         
         //Sandwiches
         Ingrediente pan = new Ingrediente("Pan", Float.valueOf(5));
@@ -64,61 +68,173 @@ public class test {
         Ingrediente aguacate = new Ingrediente("Aguacate", Float.valueOf(0));
         Ingrediente zanahoria = new Ingrediente("Zanahoria", Float.valueOf(0));
         Ingrediente cebolla = new Ingrediente("Cebolla", Float.valueOf(0));
-        Ingrediente chiles = new Ingrediente("Chiles", Float.valueOf(0));
+        Ingrediente chile = new Ingrediente("Chile", Float.valueOf(0));
         Ingrediente tomate = new Ingrediente("Tomate", Float.valueOf(5));
         
+        //Snacks
+        Ingrediente tostitos = new Ingrediente("Tostitos", Float.valueOf(0));
+        Ingrediente chips = new Ingrediente("Chips", Float.valueOf(0));
+        Ingrediente papas = new Ingrediente("Papas", Float.valueOf(0));
+        Ingrediente pepino = new Ingrediente("Pepino", Float.valueOf(0));
+        Ingrediente cueritos = new Ingrediente("Cueritos", Float.valueOf(0));
+        Ingrediente salchicha = new Ingrediente("Salchicha", Float.valueOf(0));
+        Ingrediente jicama = new Ingrediente("Jicama", Float.valueOf(0));
+        Ingrediente mango = new Ingrediente("Mango", Float.valueOf(0));
+        Ingrediente chacachaca = new Ingrediente("Chaca Chaca", Float.valueOf(0));
+        Ingrediente rielito = new Ingrediente("Rielito", Float.valueOf(0));
+        Ingrediente chamoy = new Ingrediente("Chamoy", Float.valueOf(0));
+        Ingrediente tarugo = new Ingrediente("Tarugo", Float.valueOf(0));
+        Ingrediente clamato = new Ingrediente("Clamato", Float.valueOf(0));
+        Ingrediente cacahuate = new Ingrediente("Cacahuate", Float.valueOf(0));
         
+        //Postres
+        Ingrediente manzana = new Ingrediente("Manzana", Float.valueOf(0));
+        Ingrediente pera = new Ingrediente("Pera", Float.valueOf(0));
+        Ingrediente yogurtNatural = new Ingrediente("Yogurt Natural", Float.valueOf(0));
+        Ingrediente granola = new Ingrediente("Granola", Float.valueOf(0));
+        Ingrediente miel = new Ingrediente("Miel", Float.valueOf(0));
         
-        Ingrediente milanesa = new Ingrediente("Milanesa", Float.valueOf(15));
-       
-        Ingrediente leche = new Ingrediente("Leche", Float.valueOf(0));
-       
+        //Frappes
+        Ingrediente hielo = new Ingrediente("Hielo", Float.valueOf(0));
+        Ingrediente polvoFrappeRegular = new Ingrediente("Polvo para frappe regular", Float.valueOf(0));
+        Ingrediente polvoFrappeVainilla = new Ingrediente("Polvo para frappe vainilla", Float.valueOf(0));
+        Ingrediente polvoFrappeMoka = new Ingrediente("Polvo para frappe moka", Float.valueOf(0));
+        Ingrediente galletaOreo = new Ingrediente("Galleta Oreo triturada", Float.valueOf(0));
         Ingrediente chocolate = new Ingrediente("Chocolate", Float.valueOf(10));
-        Ingrediente nieve = new Ingrediente("Nieve", Float.valueOf(10));
+        Ingrediente cremaBatida = new Ingrediente("Crema Batida", Float.valueOf(0));
+        Ingrediente galleta = new Ingrediente("Galleta", Float.valueOf(0));
+        Ingrediente chispas = new Ingrediente("Chispas", Float.valueOf(0));
+        Ingrediente jarabeCremaIrlandesa = new Ingrediente("Jarabe de crema irlandesa", Float.valueOf(0));
+        Ingrediente jarabeKahlua = new Ingrediente("Jarabe de cafe kahlua", Float.valueOf(0));
+        Ingrediente cajeta = new Ingrediente("Cajeta", Float.valueOf(0));
+        Ingrediente paletaMagnum = new Ingrediente("Paleta Magnum", Float.valueOf(0));
+        Ingrediente gansito = new Ingrediente("Gansito", Float.valueOf(0));
+
+        //Jugos
+        Ingrediente naranja = new Ingrediente("Naranja", Float.valueOf(5));       
         
-        Ingrediente agua = new Ingrediente("Agua", Float.valueOf(0));
-        Ingrediente naranja = new Ingrediente("Naranja", Float.valueOf(5));
-        Ingrediente toronja = new Ingrediente("Toronja", Float.valueOf(7));
-        
-        Ingrediente tortilla = new Ingrediente("Tortilla", Float.valueOf(3));
-        Ingrediente frijoles = new Ingrediente("Frijoles", Float.valueOf(5));
-        
-        Ingrediente cafeGrano = new Ingrediente("Cafe", Float.valueOf(0));
-        Ingrediente caramelo = new Ingrediente("Caramelo", Float.valueOf(5));
-        
+       
+
+        //PRODUCTOS
         
         //Licuados
         Producto lPlatano = new Producto("Licuado de Platano", Float.valueOf(20), 10, Categoria.LICUADOS);
         Producto lChocomilk = new Producto("Licuado de Chocomilk", Float.valueOf(20), 10, Categoria.LICUADOS);
-        Producto lChocoplatano = new Producto("Licuado de Chocomilk", Float.valueOf(20), 10, Categoria.LICUADOS);
+        Producto lChocoplatano = new Producto("Licuado de Platano/Chocomilk", Float.valueOf(20), 10, Categoria.LICUADOS);
+        Producto lFresa = new Producto("Licuado de Fresa", Float.valueOf(30), 10, Categoria.LICUADOS);
+        Producto lArandono = new Producto("Licuado de Arandono", Float.valueOf(20), 10, Categoria.LICUADOS);
+        Producto lPapaya = new Producto("Licuado de Papaya", Float.valueOf(25), 10, Categoria.LICUADOS);
+        Producto lFrutosRojos = new Producto("Licuado de Frutos Rojos", Float.valueOf(30), 10, Categoria.LICUADOS);
         
+        //Cafe
+        Producto cAmericano = new Producto("Cafe Americano", Float.valueOf(25), 10, Categoria.CAFES);
+        Producto cCapuchino = new Producto("Cafe Capuchino", Float.valueOf(30), 10, Categoria.CAFES);
+        Producto cLatte = new Producto("Cafe Latte", Float.valueOf(30), 10, Categoria.CAFES);
+        
+        //Sandwiches
+        Producto sJamon = new Producto("Sandwich jamon", Float.valueOf(25), 10, Categoria.SANDWICHES);
         Producto torta = new Producto("Torta de jamon", Float.valueOf(30), 10, Categoria.SANDWICHES);
+        Producto Panini = new Producto("Panini", Float.valueOf(30), 10, Categoria.SANDWICHES);
+        Producto PaniniEspecial = new Producto("Panini especial", Float.valueOf(40), 10, Categoria.SANDWICHES);
+        
+        //Jugos
+        Producto jNaranja = new Producto("Jugo de naranja", Float.valueOf(25), 10, Categoria.JUGOS);
+        Producto jToronja = new Producto("Jugo de toronja", Float.valueOf(25), 10, Categoria.JUGOS);
+        Producto jNZanahoria = new Producto("Jugo de narnaja/zanahoria", Float.valueOf(20), 10, Categoria.JUGOS);
+        Producto jNBetanbel = new Producto("Jugo de naranja/betabel", Float.valueOf(25), 10, Categoria.JUGOS);
+        Producto jNZBetabel = new Producto("Jugo de naranja/zanahoria/betabel", Float.valueOf(30), 10, Categoria.JUGOS);
+        Producto jNPina = new Producto("Jugo de naranja/piña", Float.valueOf(25), 10, Categoria.JUGOS);
+        
+        //Bebidas
+
+        //Snacks
+        Producto Ptostitos = new Producto("Tostitos", Float.valueOf(40), 10, Categoria.SNACKS);
+        Producto pChips = new Producto("Tostitos", Float.valueOf(45), 10, Categoria.SNACKS);
+        Producto Ppapas = new Producto("Tostitos", Float.valueOf(40), 10, Categoria.SNACKS);
+        Producto Pcacahuates = new Producto("Tostitos", Float.valueOf(30), 10, Categoria.SNACKS);
+        
+        //Postres
+        Producto escamocha = new Producto("Escamocha", Float.valueOf(30), 10, Categoria.POSTRES);
+        Producto gelatina = new Producto("Gelatina", Float.valueOf(20), 10, Categoria.POSTRES);
+        Producto nieveChocolate = new Producto("Nieve Chocolate", Float.valueOf(20), 10, Categoria.POSTRES);
+        
+        //Frappes
+        Producto fOreo = new Producto("Frappe Oreo", Float.valueOf(35), 10, Categoria.FRAPPES);
+        Producto fRegular = new Producto("Frappe regular", Float.valueOf(30), 10, Categoria.FRAPPES);
+        Producto fCIrlandesa = new Producto("Frappe crema irlandesa", Float.valueOf(35), 10, Categoria.FRAPPES);
+        Producto fMagnum = new Producto("Frappe Magnum", Float.valueOf(40), 10, Categoria.FRAPPES);
+        Producto fGansito = new Producto("Frappe Gansito", Float.valueOf(35), 10, Categoria.FRAPPES);
+        Producto fMoka = new Producto("Frappe Moka", Float.valueOf(35), 10, Categoria.FRAPPES);
+        Producto fCapuchino = new Producto("Frappe Capuchino", Float.valueOf(35), 10, Categoria.FRAPPES);
+        Producto fKahlua = new Producto("Frappe Kahlua", Float.valueOf(35), 10, Categoria.FRAPPES);
         
         Producto tacoFrijol = new Producto("Taco de frijol", Float.valueOf(20), 10, Categoria.SNACKS);
-        Producto cafe = new Producto("Cafe", Float.valueOf(25), 10, Categoria.CAFES);
-        Producto frappe = new Producto("Frappe", Float.valueOf(35), 10, Categoria.FRAPPES);
         Producto tortaMilanesa = new Producto("Torta de milanesa", Float.valueOf(50), 10, Categoria.SANDWICHES);
-        Producto jugoNaranja = new Producto("Jugo de naranja", Float.valueOf(25), 10, Categoria.JUGOS);
-        Producto jugoToronja = new Producto("Jugo de toronja", Float.valueOf(25), 10, Categoria.JUGOS);
-        Producto nieveChocolate = new Producto("Nieve de chocolate", Float.valueOf(30), 10, Categoria.POSTRES);
+        
+        
                              
         ingredientes.guardar(lechuga);
         ingredientes.guardar(tomate);
         ingredientes.guardar(jamon);
         ingredientes.guardar(queso);
         ingredientes.guardar(pan);
-        ingredientes.guardar(milanesa);
         ingredientes.guardar(leche);
         ingredientes.guardar(platano);
         ingredientes.guardar(chocolate);
-        ingredientes.guardar(nieve);
         ingredientes.guardar(agua);
-        ingredientes.guardar(naranja);
-        ingredientes.guardar(toronja);
-        ingredientes.guardar(tortilla);
-        ingredientes.guardar(frijoles);
-        ingredientes.guardar(cafeGrano);
-        ingredientes.guardar(caramelo);
+        
+        ingredientes.guardar(azucar);
+        ingredientes.guardar(gansito);
+        ingredientes.guardar(paletaMagnum);
+        ingredientes.guardar(cajeta);
+        ingredientes.guardar(jarabeKahlua);
+        ingredientes.guardar(jarabeCremaIrlandesa);
+        
+        ingredientes.guardar(chispas);
+        ingredientes.guardar(galleta);
+        ingredientes.guardar(cremaBatida);
+        ingredientes.guardar(galletaOreo);
+        ingredientes.guardar(polvoFrappeMoka);
+        ingredientes.guardar(polvoFrappeVainilla);
+        ingredientes.guardar(polvoFrappeRegular);
+        
+        ingredientes.guardar(hielo);
+        ingredientes.guardar(miel);
+        ingredientes.guardar(granola);
+        ingredientes.guardar(yogurtNatural);
+        ingredientes.guardar(pera);
+        ingredientes.guardar(manzana);
+        ingredientes.guardar(polvoFrappeVainilla);
+        
+        ingredientes.guardar(canela);
+        ingredientes.guardar(chocomilk);
+        ingredientes.guardar(fresa);
+        ingredientes.guardar(arandono);
+        ingredientes.guardar(frutosRojos);
+        ingredientes.guardar(cafe);
+        ingredientes.guardar(mayonesa);
+        ingredientes.guardar(aderezoChipotle);
+        ingredientes.guardar(carne);
+        ingredientes.guardar(aguacate);
+        
+        ingredientes.guardar(zanahoria);
+        ingredientes.guardar(cebolla);
+        ingredientes.guardar(chile);
+        ingredientes.guardar(tostitos);
+        ingredientes.guardar(chips);
+        ingredientes.guardar(papas);
+        
+        ingredientes.guardar(cueritos);
+        ingredientes.guardar(salchicha);
+        ingredientes.guardar(jicama);
+        ingredientes.guardar(mango);
+        ingredientes.guardar(chacachaca);
+        ingredientes.guardar(rielito);
+        ingredientes.guardar(chamoy);
+        ingredientes.guardar(tarugo);
+        ingredientes.guardar(clamato);
+        ingredientes.guardar(cacahuate);
+        ingredientes.guardar(pepino);
         
        
     
@@ -187,36 +303,36 @@ public class test {
     
     DetalleIngrediente diGranoCafe = new DetalleIngrediente();
     diGranoCafe.setIngrediente(ingredientes.buscarporID(16));
-    diGranoCafe.setProducto(cafe);
+    diGranoCafe.setProducto(cAmericano);
     DetalleIngrediente diLecheCafe = new DetalleIngrediente();
     diLecheCafe.setIngrediente(ingredientes.buscarporID(7));
-    diLecheCafe.setProducto(cafe);
+    diLecheCafe.setProducto(cAmericano);
     
     ArrayList<DetalleIngrediente> ingredientesCafe = new ArrayList<>();
     ingredientesCafe.add(diGranoCafe);
     ingredientesCafe.add(diLecheCafe);
     
-    cafe.setDetalleIngredientes(ingredientesCafe);
-    productos.guardar(cafe);
+    cAmericano.setDetalleIngredientes(ingredientesCafe);
+    productos.guardar(cAmericano);
     
     //
     
     DetalleIngrediente diGranoFrappe = new DetalleIngrediente();
     diGranoFrappe.setIngrediente(ingredientes.buscarporID(16));
-    diGranoFrappe.setProducto(frappe);
+    diGranoFrappe.setProducto(fOreo);
     DetalleIngrediente diLecheFrappe = new DetalleIngrediente();
     diLecheFrappe.setIngrediente(ingredientes.buscarporID(7));
-    diLecheFrappe.setProducto(frappe);
+    diLecheFrappe.setProducto(fOreo);
     DetalleIngrediente diCaramelo = new DetalleIngrediente();
     diCaramelo.setIngrediente(ingredientes.buscarporID(17));
-    diCaramelo.setProducto(frappe);
+    diCaramelo.setProducto(fOreo);
     
     ArrayList<DetalleIngrediente> ingredientesFrappe = new ArrayList<>();
     ingredientesFrappe.add(diGranoFrappe);
     ingredientesFrappe.add(diLecheFrappe);
     
-    frappe.setDetalleIngredientes(ingredientesFrappe);
-    productos.guardar(frappe);
+    fOreo.setDetalleIngredientes(ingredientesFrappe);
+    productos.guardar(fOreo);
     
     //
     
@@ -237,33 +353,33 @@ public class test {
     
     DetalleIngrediente diAguaNaranja = new DetalleIngrediente();
     diAguaNaranja.setIngrediente(ingredientes.buscarporID(11));
-    diAguaNaranja.setProducto(jugoNaranja);
+    diAguaNaranja.setProducto(jNaranja);
     DetalleIngrediente diNaranja = new DetalleIngrediente();
     diNaranja.setIngrediente(ingredientes.buscarporID(12));
-    diNaranja.setProducto(jugoNaranja);
+    diNaranja.setProducto(jNaranja);
     
     ArrayList<DetalleIngrediente> ingredientesJugoNaranja = new ArrayList<>();
     ingredientesJugoNaranja.add(diAguaNaranja);
     ingredientesJugoNaranja.add(diNaranja);
     
-    jugoNaranja.setDetalleIngredientes(ingredientesJugoNaranja);
-    productos.guardar(jugoNaranja);
+    jNaranja.setDetalleIngredientes(ingredientesJugoNaranja);
+    productos.guardar(jNaranja);
     
      //
     
     DetalleIngrediente diAguaToronja = new DetalleIngrediente();
     diAguaToronja.setIngrediente(ingredientes.buscarporID(11));
-    diAguaToronja.setProducto(jugoToronja);
+    diAguaToronja.setProducto(jToronja);
     DetalleIngrediente diToronja = new DetalleIngrediente();
     diToronja.setIngrediente(ingredientes.buscarporID(13));
-    diToronja.setProducto(jugoToronja);
+    diToronja.setProducto(jToronja);
     
     ArrayList<DetalleIngrediente> ingredientesJugoToronja = new ArrayList<>();
     ingredientesJugoToronja.add(diAguaToronja);
     ingredientesJugoToronja.add(diToronja);
     
-    jugoToronja.setDetalleIngredientes(ingredientesJugoToronja);
-    productos.guardar(jugoToronja);
+    jToronja.setDetalleIngredientes(ingredientesJugoToronja);
+    productos.guardar(jToronja);
     //
     
     DetalleIngrediente diNieve = new DetalleIngrediente();
