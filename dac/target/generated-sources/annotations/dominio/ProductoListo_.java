cdcb0cee8671a42e8746d8a8beec9eb4e7029baa
danieltr13 <daniel.trinidad205857@potros.itson.edu.mx>
2021-05-28 12:11:33 -0700

    detalles finales

--- dac/target/generated-sources/annotations/dominio/ProductoListo_.java
+++ dac/target/generated-sources/annotations/dominio/ProductoListo_.java
@@ -3,7 +3,7 @@ package dominio;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-17T20:23:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-28T12:04:47")
 @StaticMetamodel(ProductoListo.class)
 public class ProductoListo_ extends Producto_ {
 
